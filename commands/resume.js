7530b14e6205d515e72a2f372300e80020095ecb
externalxdpro <externalxdpro@gmail.com>
2021-11-21 21:08:28 -0500

    Fix a mistake

--- commands/resume.js
+++ commands/resume.js
@@ -7,6 +7,6 @@ module.exports = {
         .setDescription(`Resumes playing music`),
     async execute(interaction){
       music.resume({ interaction: interaction });
-      interaction.reply(`Stopped playback`);
+      interaction.reply(`Resumed playback`);
     }
 }
