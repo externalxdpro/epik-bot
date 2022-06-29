b42cd456efec7e75ff5a74cc0e8a175a623ffe7d
externalxdpro <externalxdpro@gmail.com>
2022-06-29 10:30:56 -0400

    Replace unpause command with resume

--- commands/pause.js
+++ commands/pause.js
@@ -2,17 +2,17 @@ const { SlashCommandBuilder } = require(`@discordjs/builders`);
 const music = require('@koenie06/discord.js-music');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName(`pause`)
-        .setDescription(`Pauses the currently playing song`),
-    async execute(interaction){
-      const isPaused = await music.isPaused({ interaction: interaction });
-      if (isPaused == true) {
-        interaction.reply(`Playback is already paused. Unpause with "/unpause"`);
-      }
-      else {
-        music.pause({ interaction: interaction });
-        interaction.reply(`Paused playback`);
-      }
+  data: new SlashCommandBuilder()
+    .setName(`pause`)
+    .setDescription(`Pauses the currently playing song`),
+  async execute(interaction) {
+    const isPaused = await music.isPaused({ interaction: interaction });
+    if (isPaused == true) {
+      interaction.reply(`Playback is already paused. Resume with "/resume"`);
     }
+    else {
+      music.pause({ interaction: interaction });
+      interaction.reply(`Paused playback`);
+    }
+  }
 }
--- commands/resume.js
+++ commands/resume.js
@@ -2,11 +2,17 @@ const { SlashCommandBuilder } = require(`@discordjs/builders`);
 const music = require('@koenie06/discord.js-music');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName(`resume`)
-        .setDescription(`Resumes playing music`),
-    async execute(interaction){
-      music.resume({ interaction: interaction });
-      interaction.reply(`Resumed playback`);
+  data: new SlashCommandBuilder()
+    .setName(`resume`)
+    .setDescription(`Resumes the currently playing song`),
+  async execute(interaction) {
+    const isPaused = await music.isPaused({ interaction: interaction });
+    if (isPaused == true) {
+      music.unpause({ interaction: interaction });
+      interaction.reply(`Resumed playback`)
     }
+    else {
+      interaction.reply(`Playback is already unpaused. Pause with "/pause"`);
+    }
+  }
 }
